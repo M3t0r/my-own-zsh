97228566cb3ef1bdb4d9b816fb165738fe4913a8
M3t0r <github@m3t0r.de>
2018-10-12 13:15:19 +0200

    Add gap and gdc aliases for git

--- 72-git.zsh
+++ 72-git.zsh
@@ -1,7 +1,7 @@
 alias g='git'
 function gb() { git fetch -q && git checkout "$@" && git pull -q }
 alias gmaster='gb master'
-alias gm='git commit -v -m'
+alias gap="git add -p"
 alias gp='git push'
 alias gs='git status'
 # http://fredkschott.com/post/2014/02/git-log-is-so-2005/
@@ -9,6 +9,7 @@ alias gs='git status'
 alias glg='git log --oneline --decorate --all --graph --simplify-by-decoration'
 alias glog='LC_ALL="en_US.UTF-8" git log --all --oneline --graph --topo-order "--pretty=format:%x09%C(yellow)%h%Creset %m %s%Cgreen%d%Creset <%C(bold blue)%an%Creset, %C(bold black)%aI%Creset>"'
 alias gd='git diff'
+alias gdc="gd --cached"
 alias gstaged="gd --cached"
 alias gmv='git mv'
 alias grm='git rm'
