ba7437e09c5e27d9570b22cf5cc454cbfe19698b
M3t0r <github@m3t0r.de>
2018-11-07 18:13:06 +0100

    Don't use sublime while using SSH

--- 00-env.zsh
+++ 00-env.zsh
@@ -2,8 +2,8 @@ export LANG="C.UTF-8"
 
 [[ $(command which nano 2> /dev/null) ]] && export EDITOR="nano"
 [[ $(command which vim 2> /dev/null) ]] && export EDITOR="vim"
-[[ $(command which subl 2> /dev/null) ]] && export EDITOR="subl -w"
-[[ $(command which subl3 2> /dev/null) ]] && export EDITOR="subl3 -w"
+[[ $(command which subl 2> /dev/null) && ! -n $SSH_CONNECTION ]] && export EDITOR="subl -w"
+[[ $(command which subl3 2> /dev/null) && ! -n $SSH_CONNECTION ]] && export EDITOR="subl3 -w"
 export VISUAL=$EDITOR
 
 export MDV_THEME="883.6584"
