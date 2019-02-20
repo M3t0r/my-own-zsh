7f37419aee880a62b47d012d224aec7c02be3a86
M3t0r <github@m3t0r.de>
2019-02-20 14:29:08 +0100

    use more platform independet form of finding a program to detect editors

--- 00-env.zsh
+++ 00-env.zsh
@@ -1,9 +1,9 @@
 export LANG="C.UTF-8"
 
-[[ $(command which nano 2> /dev/null) ]] && export EDITOR="nano"
-[[ $(command which vim 2> /dev/null) ]] && export EDITOR="vim"
-[[ $(command which subl 2> /dev/null) && ! -n $SSH_CONNECTION ]] && export EDITOR="subl -w"
-[[ $(command which subl3 2> /dev/null) && ! -n $SSH_CONNECTION ]] && export EDITOR="subl3 -w"
+[[ $(command -v nano 2> /dev/null) ]] && export EDITOR="nano"
+[[ $(command -v vim 2> /dev/null) ]] && export EDITOR="vim"
+[[ $(command -v subl 2> /dev/null) && ! -n $SSH_CONNECTION ]] && export EDITOR="subl -w"
+[[ $(command -v subl3 2> /dev/null) && ! -n $SSH_CONNECTION ]] && export EDITOR="subl3 -w"
 export VISUAL=$EDITOR
 
 export MDV_THEME="883.6584"
