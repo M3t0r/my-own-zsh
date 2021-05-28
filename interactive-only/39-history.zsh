98fa4bf4ecd2c137a57b584b42d8fc3fcdae2c9e
M3t0r <github@m3t0r.de>
2021-05-28 21:58:36 +0200

    Adapt some history options from prezto

--- interactive-only/39-history.zsh
+++ interactive-only/39-history.zsh
@@ -4,6 +4,11 @@
 
 setopt appendhistory
 
+# from prezto/modules/history/init.zsh
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
+setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
+
 HISTFILE=~/.zsh_histfile
 SAVEHIST=25000 # number of commands saved in $HISTFILE
 HISTSIZE=$SAVEHIST # number of commands available in the shell
