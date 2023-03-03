8e28dcfb45fe1ae18cb42373e02962cab5b9ac67
M3t0r <github@m3t0r.de>
2023-03-03 13:57:58 +0100

    Fix ^ keybinding
    `$terminfo[kcur1]` was empty, so the bindkey command bound forward-word
    to just '^', which meant I couldn't enter commands like `git show HEAD^3`
    because zle would move the cursor one word forward instead of just
    inserting a circumflex.
    
    Also added keybinds for home and end which don't work.

--- interactive-only/30-zle-bindings.zsh
+++ interactive-only/30-zle-bindings.zsh
@@ -14,18 +14,28 @@ insert-sudo () { zle beginning-of-line; zle -U "sudo " }
 zle -N insert-sudo
 bindkey "^[s" insert-sudo # [alt]-s
 
-bindkey "^$terminfo[kcul1]" backward-word
-bindkey "^$terminfo[kcur1]"  forward-word
+# bindkey "$terminfo[kcul1]" backward-word
+# bindkey "$terminfo[kcur1]"  forward-word
 bindkey '^[[1;5C' forward-word # [ctrl]-[right]
 bindkey '^[[1;5D' backward-word # [ctrl]-[right]
 
-# from https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/key-bindings.zsh#L59
+# from https://github.com/ohmyzsh/ohmyzsh/blob/5bf7f9c83325a6cb2752e14ca01a574dbeef206e/lib/key-bindings.zsh#L78-L90
 if [[ "${terminfo[kdch1]}" != "" ]]; then
   bindkey "${terminfo[kdch1]}" delete-char            # [Delete] - delete forward
 else
   bindkey "^[[3~" delete-char
   bindkey "^[3;5~" delete-char
   bindkey "\e[3~" delete-char
+fi
+
+# from https://github.com/ohmyzsh/ohmyzsh/blob/5bf7f9c83325a6cb2752e14ca01a574dbeef206e/lib/key-bindings.zsh#L53-L64
+# [Home] - Go to beginning of line
+if [[ -n "${terminfo[khome]}" ]]; then
+  bindkey "${terminfo[khome]}" beginning-of-line
+fi
+# [End] - Go to end of line
+if [[ -n "${terminfo[kend]}" ]]; then
+  bindkey "${terminfo[kend]}"  end-of-line
 fi
 
 # from https://github.com/mika/zsh-pony#edit-command-line-in-editor
