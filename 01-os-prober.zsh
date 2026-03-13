ddf68f601244e08ad31954d3bdf2e7ca94271c7d
M3t0r <github@m3t0r.de>
2026-03-13 16:35:20 +0100

    Fix BSD detection
    This isn't a regex, is a shell glob.

--- 01-os-prober.zsh
+++ 01-os-prober.zsh
@@ -3,7 +3,7 @@ typeset OSMac OSBSD OSLinux OSUnkown # set defaults
 case $kernel in
 	"Darwin") typeset OSMac="true" OSBSD="true";;
 	"Linux") typeset OSLinux="true";;
-	.*[Bb][Ss][Dd].*) typeset OSBSD="true";; # <-- FIXME does NOT work
+	*[Bb][Ss][Dd]*) typeset OSBSD="true";;
 	*) typeset OSUnkown="true";; # if nothing matched
 esac
 unset kernel
